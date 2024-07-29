d48f42f3f72d722541112967d36af060cb9396b4
kgaft <kgaftkgaft@gmail.com>
2024-07-29 14:49:13 +0300

    updates

--- sync-commit.sh
+++ sync-commit.sh
@@ -1,4 +1,5 @@
-cp /etc/portage/make.conf etc/portage/make.conf
+#cp /etc/portage/make.conf etc/portage/make.conf
 cp -rfd /etc/portage/env etc/portage/env
 cp -rfd /etc/portage/package.env etc/portage/package.env
 cp -rfd /var/lib/portage/world var/lib/portage/world
+cp -rfd /etc/portage/package.mask etc/portage/package.mask
--- sync-install.sh
+++ sync-install.sh
@@ -2,3 +2,4 @@ cp etc/portage/make.conf /etc/portage/make.conf
 cp -rfd etc/portage/env /etc/portage/env
 cp -rfd etc/portage/package.env /etc/portage/package.env
 cp -rfd var/lib/portage/world /var/lib/portage/world
+cp -rfd etc/portage/package.mask /etc/portage/package.mask
